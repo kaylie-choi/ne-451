a85f695c2a45cd57596c652fd84c4480930ba746
kaylie-choi <k33choi@uwaterloo.ca>
2021-10-17 01:19:38 -0400

    pk fixed

---
+++ NE451_Assignment3/NE451_Assignment3/451_A3_P1.cpp
@@ -0,0 +1,33 @@
+//
+//  NE451 Assignment3 Problem 1
+//
+//  2021-10-16
+//  Kaylie Choi
+//  20692499
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Semiconductor {
+//    double iElectronDensity, iHoleDensity, iIntrinsicCarrierDensity;
+//
+//    public:
+//    double electronDensity();
+//        double holeDensity()
+//        double intrinsicCarrierDensity()       // read-only
+//
+//        void setElectronDensity( double aElectronDensity )
+//        void setHoleDensity( double aHoleDensity )
+//        void print()
+//
+//        Semiconductor( double aIntrinsicCarrierDensity )
+//};
+//
+//
+//int main() {
+//    Semiconductor Silicon( 1.5E10 );
+//    Silicon.setElectronDensity( 1.e10 );
+//    Silicon.print( );
+//}
---
+++ NE451_Assignment3/NE451_Assignment3/451_A3_P2.cpp
@@ -0,0 +1,45 @@
+//
+//  NE451 Assignment3 Problem 2
+//
+//  2021-10-16
+//  Kaylie Choi
+//  20692499
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+    // private double precision members iLength/iWidth
+    private:
+        double iLength, iWidth;
+    
+    public:
+        // public member functions to get and set iLength/iWidth
+        double getLength() { return iLength; }
+        double getWidth() { return iWidth; }
+    
+        void setLength( double uLength ) {
+            iLength = uLength;
+        }
+        void setWidth( double uWidth ) {
+            iWidth = uWidth;
+        }
+        
+        // 2 arg constructor setting length and width to user specified uLength/uWidth
+        Rectangle( double uLength, double uWidth ){
+            iLength = uLength;
+            iWidth = uWidth;
+        }
+    
+        // void function to calculate area, printing it through cout
+        void area() {
+            cout << getLength() * getWidth();
+        }
+};
+
+int main() {
+    Rectangle R( 10, 20 );
+    R.area();
+}
