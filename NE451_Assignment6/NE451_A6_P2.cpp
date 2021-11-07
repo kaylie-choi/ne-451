9aab63726a49bb5444f90a3a1cacaa36b41b9018
Kaylie Choi <38885193+kaylie-choi@users.noreply.github.com>
2021-11-07 00:06:19 -0400

    fix p2

--- NE451_Assignment6/NE451_A6_P2.cpp
+++ NE451_Assignment6/NE451_A6_P2.cpp
@@ -1,4 +1,4 @@
-//  NE451 Assignment 6 Problem 1
+//  NE451 Assignment 6 Problem 2
 //  2021-11-06
 //  Kaylie Choi - 20692499
 
@@ -42,22 +42,23 @@ int main ( ) {
     xPow3[loop] = pow(DC1.dx( ), 3); // deltaX raised to 3rd power
     xPow4[loop] = pow(DC1.dx( ), 4); //deltaX raised to 4th power
     y[loop] = DC1.deriv( 1.0 ); 
-    error[loop] = log(abs(DC1.deriv( 1.0 ) - 4)); // verify 4th order accuracy
-    logDeltaX[loop] = log(DC1.dx()); //log(deltaX) ; slope should be 4 near origin
+    error[loop] = log(fabs(DC1.deriv( 1.0 ) - 4)); // verify 4th order accuracy
+    logDeltaX[loop] = log(DC1.dx( )); //log(deltaX) ; slope should be 4 near origin
     DC1.setDx( DC1.dx( ) / 1.3 ); 
   }
 
+	
   // dislin plotting
 
   // graph #1 - derivative against xPow3
-  metafl( “XWIN” ); // write to terminal
+  metafl("TIFF"); // write to terminal
   disini(); // start plot
-  name( "Third Power of Step Length", “x” ); // x label
-  name( “Derivative”, “y” ); // y label
-  labels( “EXP”, ”xy” ); // exponential format
+  name( "Third Power of Step Length", "x" ); // x label
+  name( "Derivative", "y" ); // y label
+  labels( "EXP", "xy" ); // exponential format
   incmrk( 1 ); // marker every 1 point
-  setscl( xPow3, 10, “x” ); // auto scale axes
-  setscl( y, 10, “y” );
+  setscl( xPow3, 10, "x" ); // auto scale axes
+  setscl( y, 10, "y" );
   double minX, maxX, minY, maxY, stepX, stepY;
   graf( minX, maxX, minX, stepX, minY, maxY, minY, stepY ); // draw axes 
   curve( xPow3, y, 10 );  // plot curve
@@ -65,30 +66,30 @@ int main ( ) {
 
 
   // graph #2 - derivative against xPow4
-  metafl( “XWIN” ); // write to terminal
+  metafl( "TIFF" ); // write to terminal
   disini(); // start plot
-  name( "Fourth Power of Step Length", “x” ); // x label
-  name( “Derivative”, “y” ); // y label
-  labels( “EXP”, ”xy” ); // exponential format
+  name( "Fourth Power of Step Length", "x" ); // x label
+  name( "Derivative", "y" ); // y label
+  labels( "EXP", "xy" ); // exponential format
   incmrk( 1 ); // marker every 1 point
-  setscl( xPow4, 10, “x” ); // auto scale axes
-  setscl( y, 10, “y” );
-  double minX, maxX, minY, maxY, stepX, stepY;
-  graf( minX, maxX, minX, stepX, minY, maxY, minY, stepY ); // draw axes 
+  setscl( xPow4, 10, "x" ); // auto scale axes
+  setscl( y, 10, "y" );
+  double minX2, maxX2, minY2, maxY2, stepX2, stepY2;
+  graf( minX2, maxX2, minX2, stepX2, minY2, maxY2, minY2, stepY2 ); // draw axes 
   curve( xPow4, y, 10 );  // plot curve
   disfin(); // terminate plot
 
   // graph #3 - x-logdelx; y-error
-  metafl( “XWIN” ); // write to terminal
+  metafl( "TIFF" ); // write to terminal
   disini(); // start plot
-  name( "Log(delta X)", “x” ); // x label
-  name( "Logarithm of Error |df/dx - 4|" , “y” ); // y label
-  labels( LOG, ”xy” ); // exponential format
+  name( "Logarithm of deltaX", "x" ); // x label
+  name( "Logarithm of Error", "y" ); // y label
+  labels( "EXP", "xy" ); // exponential format
   incmrk( 1 ); // marker every 1 point
-  setscl( logDeltaX, 10, “x” ); // auto scale axes
-  setscl( error, 10, “y” );
-  double minX, maxX, minY, maxY, stepX, stepY;
-  graf( minX, maxX, minX, stepX, minY, maxY, minY, stepY ); // draw axes 
+  setscl( logDeltaX, 10, "x" ); // auto scale axes
+  setscl( error, 10, "y" );
+  double minX3, maxX3, minY3, maxY3, stepX3, stepY3;
+  graf( minX3, maxX3, minX3, stepX3, minY3, maxY3, minY3, stepY3 ); // draw axes 
   curve( logDeltaX, error, 10 );  // plot curve
   disfin(); // terminate plot
 }
